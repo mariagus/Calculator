0582b380fb5860822c6b4960974952930d373eac
Maria Gusova <masha.gu@gmail.com>
2020-08-22 19:38:19 +0200

    Updates

---
+++ precache-manifest.c5683a9e79e871fd3243936ba30a0ef2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5020e5a1967e8dc514082cbd74fdb0b",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "f5336320b39e21276d92",
+    "url": "/calculator/static/css/main.e21d830e.chunk.css"
+  },
+  {
+    "revision": "198e0bf36498faa8c0c8",
+    "url": "/calculator/static/js/2.0a46f8d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.0a46f8d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5336320b39e21276d92",
+    "url": "/calculator/static/js/main.fe4c449b.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
